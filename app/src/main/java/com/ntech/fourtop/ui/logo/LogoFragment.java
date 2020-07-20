eb25805cc1a36a622b1c9a39ba9231cb06d5e96c
SuperCasual <evg.mikhalets@gmail.com>
2020-07-20 23:43:49 +0300

    fix logo view and token request

--- app/src/main/java/com/ntech/fourtop/ui/logo/LogoFragment.java
+++ app/src/main/java/com/ntech/fourtop/ui/logo/LogoFragment.java
@@ -1,7 +1,6 @@
 package com.ntech.fourtop.ui.logo;
 
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -16,15 +15,15 @@ import androidx.navigation.Navigation;
 
 import com.ntech.fourtop.R;
 import com.ntech.fourtop.databinding.FragmentLogoBinding;
-import com.ntech.fourtop.ui.MainActivity;
 import com.ntech.fourtop.utils.Const;
 
 import org.jetbrains.annotations.NotNull;
 
 public class LogoFragment extends Fragment {
 
-    private FragmentLogoBinding binding;
     private LogoViewModel viewModel;
+    private FragmentLogoBinding binding;
+    private String userToken;
 
     @Override
     public View onCreateView(@NotNull LayoutInflater inflater,
@@ -38,18 +37,15 @@ public class LogoFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         viewModel = new ViewModelProvider(requireActivity()).get(LogoViewModel.class);
+        viewModel.getThrowable().observe(getViewLifecycleOwner(), this::throwableObserver);
+        viewModel.getErrorMessage().observe(getViewLifecycleOwner(), this::errorMessageObserver);
+        viewModel.getLiveDataResponse().observe(getViewLifecycleOwner(), this::responseObserver);
+    }
 
-        viewModel.getApiToken().observe(getViewLifecycleOwner(), s -> {
-            if (s.equals("VALID")) {
-                openMainScreen(viewModel.getUserToken().getValue());
-            } else if (s.equals("INVALID")) {
-                openLoginScreen();
-            } else {
-                binding.textError.setText(s);
-            }
-        });
-
-        loadUserToken();
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (userToken == null) loadUserToken();
     }
 
     @Override
@@ -58,37 +54,41 @@ public class LogoFragment extends Fragment {
         binding = null;
     }
 
-    /**
-     * Get user token from SharedPreferences and set it in viewModel.
-     */
+    private void throwableObserver(String throwable) {
+        binding.textError.setText(throwable);
+    }
+
+    private void errorMessageObserver(String throwable) {
+        navigateToLogin();
+    }
+
+    private void responseObserver(int status) {
+        navigateToHome();
+    }
+
+    // Get user token from SharedPreferences and set it in viewModel.
     private void loadUserToken() {
         SharedPreferences sp = requireActivity().getSharedPreferences(
                 Const.SHARED_FILE, Context.MODE_PRIVATE);
-        String token = sp.getString(Const.SHARED_TOKEN, "");
-        if (!token.isEmpty()) {
-            viewModel.getUserToken().setValue(token);
-            viewModel.tokenRequest(token);
+        userToken = sp.getString(Const.SHARED_TOKEN, "");
+
+        if (userToken != null && !userToken.isEmpty()) {
+            viewModel.tokenRequest(userToken);
         } else {
-            openLoginScreen();
+            navigateToLogin();
         }
     }
 
-    /**
-     * Go to MainActivity if token is exist
-     * @param token user identifier string for interacting with the server
-     */
-    private void openMainScreen(String token) {
-        if (token != null && !token.isEmpty()) {
-            Intent intent = new Intent(getContext(), MainActivity.class);
-            intent.putExtra(Const.ARGS_TOKEN, token);
-            startActivity(intent);
-        }
+    // Navigate to home (news) view
+    private void navigateToHome() {
+        Bundle args = new Bundle();
+        args.putString(Const.ARGS_TOKEN, userToken);
+        Navigation.findNavController(binding.getRoot())
+                .navigate(R.id.action_logoFragment_to_homeFragment, args);
     }
 
-    /**
-     * Navigate to LoginFragment
-     */
-    private void openLoginScreen() {
+    // Navigate to login view
+    private void navigateToLogin() {
         Navigation.findNavController(binding.getRoot())
                 .navigate(R.id.action_logoFragment_to_loginFragment);
     }
--- app/src/main/java/com/ntech/fourtop/ui/logo/LogoViewModel.java
+++ app/src/main/java/com/ntech/fourtop/ui/logo/LogoViewModel.java
@@ -4,27 +4,74 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.ntech.fourtop.data.AppRepository;
+import com.ntech.fourtop.network.pojo.ResponseBase;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+import timber.log.Timber;
+
 public class LogoViewModel extends ViewModel {
 
-    private LogoRepository logoRepository;
-    private MutableLiveData<String> apiToken;
-    private MutableLiveData<String> userToken;
+    private AppRepository repository;
+    private CompositeDisposable disposables;
+    private MutableLiveData<String> throwable;
+    private MutableLiveData<String> errorMessage;
+    private MutableLiveData<Integer> liveDataResponse;
 
     public LogoViewModel() {
-        logoRepository = new LogoRepository();
-        apiToken = new MutableLiveData<>();
-        userToken = new MutableLiveData<>();
+        repository = AppRepository.get();
+        disposables = new CompositeDisposable();
+        throwable = new MutableLiveData<>();
+        errorMessage = new MutableLiveData<>();
+        liveDataResponse = new MutableLiveData<>();
     }
 
-    public LiveData<String> getApiToken() {
-        return apiToken;
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        disposables.clear();
     }
 
-    public MutableLiveData<String> getUserToken() {
-        return userToken;
+    public LiveData<String> getThrowable() {
+        return throwable;
+    }
+
+    public LiveData<String> getErrorMessage() {
+        return errorMessage;
+    }
+
+    public LiveData<Integer> getLiveDataResponse() {
+        return liveDataResponse;
     }
 
     public void tokenRequest(String token) {
-        logoRepository.tokenRequest(token, result -> apiToken.setValue(result));
+        Timber.d("Send token request");
+        Disposable disposable = repository.tokenRequest(token)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(this::onSuccess, this::onError);
+        disposables.add(disposable);
+    }
+
+    private void onSuccess(ResponseBase response) {
+        int status = response.getStatus();
+        Timber.d("Request status %d", status);
+
+        switch (status) {
+            case 200:
+                liveDataResponse.setValue(status);
+                break;
+            case 403:
+                errorMessage.setValue(response.getErrorMsg());
+                break;
+        }
+    }
+
+    private void onError(Throwable t) {
+        Timber.d(t);
+        throwable.setValue(t.toString());
     }
 }
